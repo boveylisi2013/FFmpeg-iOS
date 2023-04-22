52c456ea2983e2ba3cdaec74a8c6bd61e271de48
Si Li <5580508+boveylisi2013@users.noreply.github.com>
2023-04-21 20:06:15 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "FFmpeg-iOS",
-    platforms: [.iOS(.v13), .macOS(.v11)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "FFmpeg-iOS",
